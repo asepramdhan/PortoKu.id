5b3fe7153c5d1bca5fbb0866251cf073256dc51c
Asep Ramdan <80875965+asepramdhan@users.noreply.github.com>
2025-09-03 17:30:52 +0700

    update
    update

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -32,20 +32,6 @@
       0 => 'Laravel\\Folio\\FolioServiceProvider',
     ),
   ),
-  'laravel/pail' => 
-  array (
-    'providers' => 
-    array (
-      0 => 'Laravel\\Pail\\PailServiceProvider',
-    ),
-  ),
-  'laravel/sail' => 
-  array (
-    'providers' => 
-    array (
-      0 => 'Laravel\\Sail\\SailServiceProvider',
-    ),
-  ),
   'laravel/socialite' => 
   array (
     'aliases' => 
@@ -94,13 +80,6 @@
     'providers' => 
     array (
       0 => 'Carbon\\Laravel\\ServiceProvider',
-    ),
-  ),
-  'nunomaduro/collision' => 
-  array (
-    'providers' => 
-    array (
-      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
     ),
   ),
   'nunomaduro/termwind' => 
